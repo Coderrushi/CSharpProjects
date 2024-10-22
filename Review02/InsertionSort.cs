c3b8a973c9e28087d9a74356754c56b26517f3e2
Hrushikesh Zarekar <rushikeshzarekar265@gmail.com>
2024-10-23 00:07:03 +0530

    added review2

---
+++ Review02/InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review02
+{
+    internal class InsertionSort
+    {
+        public static void Insertionsort(int[] arr)
+        {
+            int n = arr.Length;
+            for(int i = 1; i < n; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while(j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+            Console.WriteLine("After Insertion sort: ");
+            for(int i=0; i <n; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+    }
+}
---
+++ Review02/Review02.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace Review02
+{
+    internal class Program
+    {
+        static int FindMaxEle(int[] arr)
+        {
+            int maxSum = arr[0], currSum = arr[0];
+            int start = 0;
+            int end = 0;
+            int tempStart = 0;
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > currSum + arr[i])
+                {
+                    currSum = arr[i];
+                    tempStart = i;
+                }
+                else
+                {
+                    currSum += arr[i];
+                }
+                if(currSum > maxSum)
+                {
+                    maxSum = currSum;
+                    start = tempStart;
+                    end = i;
+                }
+            }
+            return maxSum;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 1, 3, 4, 2};
+            InsertionSort.Insertionsort(arr);
+            //int maxSum = FindMaxEle(arr);
+            //Console.WriteLine($"Maximum sum is: {maxSum}");
+            
+        }
+    }
+}
