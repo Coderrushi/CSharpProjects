9ff16aa3f8f7ccfa4152fb6926a75f789f394e88
Hrushikesh Zarekar <rushikeshzarekar265@gmail.com>
2024-10-23 00:08:49 +0530

    implemented stack using linked list

---
+++ StackImplUsingLL/Stack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StackImplUsingLL
+{
+    internal class Stack
+    {
+        private Node top;
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data) 
+            {
+                this.data = data;
+                this.next = null;
+            }
+        }
+        public void Push(int new_data)
+        {
+            Node newNode = new Node(new_data);
+            if(top == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+            Console.WriteLine(new_data + " pushed to stack ");
+        }
+        public void Pop()
+        {
+            if(top == null)
+            {
+                Console.WriteLine("\n Stack is underflow");
+                return;
+            }
+            Console.WriteLine(top.data + " is popped");
+            top = top.next;
+        }
+        public void Peek()
+        {
+            if(top == null)
+            {
+                Console.WriteLine("Stack is underflow");
+                return;
+            }
+            Console.WriteLine(top.data + " is on the top of the stack"); 
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Node temp = top;
+            Console.WriteLine("Stack elements are: ");
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StackImplUsingLL/StackImplUsingLL.cs
@@ -0,0 +1,20 @@
+﻿namespace StackImplUsingLL
+{
+    internal class StackImplUsingLL
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            stack.Push(50);
+            stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.Peek();
+            stack.Display();
+        }
+    }
+}
