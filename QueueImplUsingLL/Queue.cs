7420be50c019e717a57a6bec22c0d068564370fb
Hrushikesh Zarekar <rushikeshzarekar265@gmail.com>
2024-10-23 00:09:46 +0530

    Implemented queue using linked list

---
+++ QueueImplUsingLL/Queue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplUsingLL
+{
+    internal class Queue
+    {
+        Node front;
+        Node rear;
+        public Queue()
+        {
+            this.front = this.rear = null;
+        }
+        internal class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                this.data = d;
+                next = null;
+            }
+        }
+        internal void Enqueue(int item)
+        {
+            Node newNode = new Node(item);
+            if (this.rear == null)
+            {
+                this.front = this.rear = newNode;
+            }
+            else
+            {
+                this.rear.next = newNode;
+                this.rear = newNode;
+            }
+            Console.WriteLine(item + "inserted into Queue");
+        }
+        internal void Dequeue()
+        {
+            if(this.front == null)
+            {
+                Console.WriteLine("The queue is empty");
+                return;
+            }
+            Node temp = this.front;
+            this.front = this.front.next;
+            if(this.front == null)
+            {
+                this.rear = null;
+            }
+            Console.WriteLine(temp.data + " is deleted");
+        }
+        internal void Dispay() 
+        {
+            if(front == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            Node temp = front;
+            Console.WriteLine("Queue elements are: ");
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ QueueImplUsingLL/QueueImplUsingLL.cs
@@ -0,0 +1,18 @@
+﻿namespace QueueImplUsingLL
+{
+    internal class QueueImplUsingLL
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Dispay();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dispay();
+        }
+    }
+}
